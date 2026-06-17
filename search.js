87504fb42827bc9fb2e2c28f8777e827a93354a8
mkxdevx <mikaylaesparza06@gmail.com>
2026-06-17 00:17:29 -0500

    filter

--- search.js
+++ search.js
@@ -1,9 +1,13 @@
 const moviesListEl = document.querySelector('.movies');
 const input = document.querySelector('.search__input');
 const searchButton = document.querySelector('.search__btn');
+const resultsHeaderEl = document.querySelector('.results__header--title');
+const filterEl = document.querySelector('#filter');
+let currentSearchTerm = "";
 
 
-async function searchMovies(searchTerm) {
+async function searchMovies(searchTerm, showHeader = true) {
+    currentSearchTerm = searchTerm;
     const movies = await fetch(`https://www.omdbapi.com/?s=${searchTerm}&apikey=a314fffb`);
     const moviesData = await movies.json();
 
@@ -13,10 +17,16 @@ async function searchMovies(searchTerm) {
         searchButton.classList.remove("loading");
     }
 
-    moviesListEl.innerHTML = moviesData.Search.map(movie => movieData(movie)).join('');
+    let moviesSearchList = moviesData.Search;
+    moviesSearchList = filterMovies(moviesSearchList);
+    moviesListEl.innerHTML = moviesSearchList.map(movie => movieData(movie)).join('');
+
+    if(showHeader) {
+        resultsHeaderEl.innerHTML = `<h1 class="results__header--title">Search results for <span class="crimson">"${searchTerm}"</span></h1>`;
+    }
 }
 
-searchMovies("movie");
+searchMovies("movie", false);
 
 function movieData(movie) {
     return `<div class="movie">
@@ -25,6 +35,33 @@ function movieData(movie) {
         <h3 class="movie__title">${movie.Title}</h3>
         <p class="movie__year">${movie.Year}</p>
     </div>`;
+}
+
+function filterMovies(movies) {
+    const value = filterEl.value;
+
+    if(value === 'A_TO_Z') {
+        movies.sort((a, b) => a.Title.toLowerCase().localeCompare(b.Title.toLowerCase()));
+    }
+
+    if(value === 'Z_TO_A') {
+        movies.sort((a, b) => b.Title.toLowerCase().localeCompare(a.Title.toLowerCase()));
+    }
+
+    if(value === 'NEW_TO_OLD') {
+        movies.sort((a, b) => b.Year - a.Year);
+    }
+
+    if(value === 'OLD_TO_NEW') {
+        movies.sort((a, b) => a.Year - b.Year);
+    }
+    return movies;
+}
+
+function handleFilterChange(event) {
+    if(currentSearchTerm) {
+        searchMovies(currentSearchTerm);
+    }
 }
 
 function delay(ms) {
@@ -39,13 +76,14 @@ async function handleSearch(event) {
 
     await delay(200);
 
+
     searchButton.classList.remove("loading");
 
 }
 
 input.addEventListener('keydown', (e) => {
     if (e.key === 'Enter') {
-        return handleSearch(event);
+        handleSearch(e);
     }
 });
 
